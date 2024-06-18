9401e19311edf8c62f01d02e3ea94ef9b857dff7
GK-2308 <geetanshkhurana2@gmail.com>
2024-06-18 12:35:48 +0530

    fix

--- netflix-gpt/src/utils/constants.js
+++ netflix-gpt/src/utils/constants.js
@@ -22,6 +22,3 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "hindi", name: "Hindi" },
   { identifier: "spanish", name: "Spanish" },
 ];
-
-// export const OPENAI_KEY = import.meta.env.VITE_OPENAI_KEY;
-//   "";
